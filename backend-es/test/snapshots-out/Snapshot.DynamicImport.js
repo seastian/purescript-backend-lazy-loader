24acb40791771da769e2fec24e4c13dc99707b90
Rory Campbell <roryc89@googlemail.com>
2023-10-10 06:16:43 +0200

    adds DynamicImport runtime test

--- backend-es/test/snapshots-out/Snapshot.DynamicImport.js
+++ backend-es/test/snapshots-out/Snapshot.DynamicImport.js
@@ -1,2 +1,20 @@
+import * as Assert from "../Assert/index.js";
+import * as Control$dPromise from "../Control.Promise/index.js";
+import * as Data$dEq from "../Data.Eq/index.js";
+import * as Data$dShow from "../Data.Show/index.js";
+import * as Effect$dAff from "../Effect.Aff/index.js";
 const lazyCharCode = (() => import("../Data.Enum/index.js").then(Data$dEnum => Data$dEnum.toCharCode));
-export {lazyCharCode};
+const main = /* #__PURE__ */ (() => {
+  const $0 = Effect$dAff._makeFiber(
+    Effect$dAff.ffiUtil,
+    Effect$dAff._bind(Effect$dAff._bind(Effect$dAff._liftEffect(lazyCharCode))(Control$dPromise.toAff$p(Control$dPromise.coerce)))(charCode => Effect$dAff._liftEffect(Assert.assertEqual(Data$dEq.eqInt)(Data$dShow.showInt)("DynamicImport/lazyCharCode")({
+      expected: 97,
+      actual: charCode("a")
+    })))
+  );
+  return () => {
+    const fiber = $0();
+    fiber.run();
+  };
+})();
+export {lazyCharCode, main};
--- backend-es/test/snapshots/Snapshot.DynamicImport.purs
+++ backend-es/test/snapshots/Snapshot.DynamicImport.purs
@@ -1,9 +1,22 @@
-module Snapshot.DynamicImport (lazyCharCode) where
+module Snapshot.DynamicImport (lazyCharCode, main) where
 
-import Control.Promise (Promise)
+import Prelude
+
+import Assert (assertEqual)
+import Control.Promise (Promise, toAffE)
 import Data.Char (toCharCode)
 import DynamicImport (dynamicImport)
 import Effect (Effect)
+import Effect.Aff (launchAff_)
+import Effect.Class (liftEffect)
 
 lazyCharCode :: Effect (Promise (Char -> Int))
 lazyCharCode = dynamicImport toCharCode
+
+main :: Effect Unit
+main = launchAff_ do
+  charCode <- toAffE lazyCharCode
+  liftEffect $ assertEqual "DynamicImport/lazyCharCode"
+    { expected: 97
+    , actual: charCode 'a'
+    }
